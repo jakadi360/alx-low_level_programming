d8d5fbc5ecc3ca4a1f0336070c90b46ea4fe3f35
jakadi360 <amaturahmanali@gmail.com>
2022-07-01 13:39:51 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+
+int main(void)
+
+{
+char lowercase;
+
+for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+{
+putchar(lowercase);
+}
+
+putchar('\n');
+
+return (0);
+
+}
