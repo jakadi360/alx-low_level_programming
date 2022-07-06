1b6ce1138360299a6ed6e0df094bf6fd9a124044
jakadi360 <amaturahmanali@gmail.com>
2022-07-06 02:50:45 -0700

    task1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,13 @@
-void print_alphabet(void);
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+
+return (write(1, &c, 1));
+}
