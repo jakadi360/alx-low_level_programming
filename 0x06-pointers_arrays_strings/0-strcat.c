7cf46a1160f0aafbe23191109e7cfc8054bba0ec
jakadi360 <amaturahmanali@gmail.com>
2022-07-15 02:29:31 -0700

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,19 +11,19 @@ char *_strcat(char *dest, char *src)
 int i, j;
 
 i = 0;
-while (dest [i] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
 
 j = 0;
-while (src [j] != '\0')
+while (src[j] != '\0')
 {
-dest [i] = src [j];
+dest[i] = src[j];
 j++;
 i++;
 }
-dest [i] = '\0';
+dest[i] = '\0';
 
 return (dest);
 }
