50531dd7844e5da77e5079aa8b31189d44a18915
jakadi360 <amaturahmanali@gmail.com>
2022-07-16 01:24:58 -0700

    task5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: string
- * Returns: returns char
+ * Return: returns char
  */
 
 char *string_toupper(char *str)
