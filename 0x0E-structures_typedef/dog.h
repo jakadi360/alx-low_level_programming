1a26a64dfd3c2116a9d45847171b2d1b977a0b61
jakadi360 <amaturahmanali@gmail.com>
2022-08-01 04:11:02 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - Dogs attributes
@@ -25,5 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-return (0);
+#endif
 }
