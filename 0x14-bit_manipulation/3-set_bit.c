e8148d6d021943a79a1d7ec08be6661505c16e8a
jakadi360 <amaturahmanali@gmail.com>
2022-08-18 10:45:43 -0700

    task3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,11 +5,12 @@
  * @n: a pointer to the bit
  * @index: the index to set the value at - indices start at 0
  *
- * return: if an error occurs - -1 otherwise -1
+ * return: if an error occurs - -1
+ *         otherwise -1
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-if (index >= (sizeof(unsigned long int) *s))
+if (index >= (sizeof(unsigned long int) * 8))
 return (-1);
 
 *n ^= (1 << index);
